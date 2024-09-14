57ca17330ae5fd829770c539900002ace545f26a
t-34400 <34400mt@gmail.com>
2024-09-15 02:02:06 +0900

    Allow foreground service notification dismissal

--- app/src/main/java/com/t34400/mediaprojectionlib/core/MediaProjectionService.kt
+++ app/src/main/java/com/t34400/mediaprojectionlib/core/MediaProjectionService.kt
@@ -4,6 +4,7 @@ import android.app.Activity.RESULT_OK
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
@@ -19,29 +20,49 @@ import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import kotlin.math.roundToInt
 
+
 class MediaProjectionService : Service() {
     private val binder = LocalBinder()
 
-    private lateinit var projection: MediaProjection
-    private lateinit var virtualDisplay: VirtualDisplay
-    private lateinit var imageReader: ImageReader
+    private var projection: MediaProjection? = null
+    private var virtualDisplay: VirtualDisplay? = null
+    private var imageReader: ImageReader? = null
 
     override fun onCreate() {
         super.onCreate()
+
         createNotificationChannel()
+
+        val stopIntent = Intent(this, MediaProjectionService::class.java).apply {
+            action = ACTION_STOP_SERVICE
+        }
+        val stopPendingIntent = PendingIntent.getService(this, 0, stopIntent, PendingIntent.FLAG_IMMUTABLE)
 
         val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
             .setContentTitle("Media Projection Service")
             .setContentText("Service is running")
+            .setSmallIcon(com.t34400.mediaprojectionlib.R.drawable.baseline_smart_display_24)
+            .addAction(NotificationCompat.Action.Builder(
+                com.t34400.mediaprojectionlib.R.drawable.baseline_stop_24,
+                "Stop Service",
+                stopPendingIntent
+            ).build())
             .build()
 
-        startForeground(1, notification)
+        startForeground(NOTIFICATION_ID, notification)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        if (intent == null) {
-            return super.onStartCommand(intent, flags, startId)
-        }
+        intent?.let {
+            when (it.action) {
+                ACTION_STOP_SERVICE -> {
+                    stopSelf()
+                    return START_NOT_STICKY
+                }
+                else -> {}
+            }
+        } ?: return START_NOT_STICKY
+
         val data = if (Build.VERSION.SDK_INT >= 33) {
             intent.getParcelableExtra(KEY_DATA, Intent::class.java)
         } else {
@@ -63,34 +84,43 @@ class MediaProjectionService : Service() {
         val height = (rawHeight * scale).roundToInt()
 
         imageReader = ImageReader.newInstance(width, height, PixelFormat.RGBA_8888, 2)
-        val imageSurface = imageReader.surface
 
-        virtualDisplay = projection.createVirtualDisplay(
-            "Projection",
-            width,
-            height,
-            metrics.densityDpi,
-            DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-            imageSurface,
-            null,
-            null
-        )
+        imageReader?.let { imageReader ->
+            projection?.let { projection ->
+                val imageSurface = imageReader.surface
+
+                virtualDisplay = projection.createVirtualDisplay(
+                    "Projection",
+                    width,
+                    height,
+                    metrics.densityDpi,
+                    DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                    imageSurface,
+                    null,
+                    null
+                )
+            }
+        }
 
         return START_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        virtualDisplay.release()
-        imageReader.close()
-        projection.stop()
+
+        virtualDisplay?.release()
+        imageReader?.close()
+        projection?.stop()
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.cancel(NOTIFICATION_ID)
     }
 
     override fun onBind(intent: Intent?): IBinder {
         return binder
     }
 
-    fun getImageReader(): ImageReader {
+    fun getImageReader(): ImageReader? {
         return imageReader
     }
 
@@ -111,5 +141,7 @@ class MediaProjectionService : Service() {
     companion object {
         const val KEY_DATA = "SCREEN_CAPTURE_INTENT"
         private const val CHANNEL_ID = "MediaProjectionServiceChannel"
+        private const val NOTIFICATION_ID = 34214218
+        private const val ACTION_STOP_SERVICE = "com.t34400.mediaprojectionlib.core.STOP_SERVICE"
     }
 }
